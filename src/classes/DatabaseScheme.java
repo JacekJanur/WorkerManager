87e68956440ee1481e2f368bb4cf0ccf6883e83a
JacekJanur <jacekjanur1@gmail.com>
2021-12-31 11:59:17 +0100

    Class for adding DB structure

--- src/classes/DatabaseScheme.java
+++ src/classes/DatabaseScheme.java
@@ -21,8 +21,14 @@ public class DatabaseScheme {
 			statement = connection.createStatement();
 			
 	        statement.setQueryTimeout(30);  // set timeout to 30 sec.
-	        statement.executeUpdate("drop table if exists person");
-	        statement.executeUpdate("create table person (id integer, name string)");
+	        statement.executeUpdate("drop table if exists persons");
+	        statement.executeUpdate("drop table if exists responsibilities");
+	        statement.executeUpdate("drop table if exists persons_responsibilities");
+	        statement.executeUpdate("create table persons (id_person integer not null primary key, name string(30), surname string(30), birth date, sex BOOLEAN, salary float)");
+	        statement.executeUpdate("create table responsibilities (id_responsibility integer not null primary key, responsibility text)");
+	        statement.executeUpdate("create table persons_responsibilities (id_person integer not null, id_responsibility integer not null, "
+	        		+ "FOREIGN KEY(id_person) REFERENCES persons(id_person), "
+	        		+ "FOREIGN KEY(id_responsibility) REFERENCES responsibilities(id_responsibility))");
 		}catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/main.java
+++ src/main.java
@@ -1,11 +1,16 @@
 
 import classes.DatabaseConnection;
+import classes.DatabaseScheme;
 
 public class main
 {
 	public static void main(String[] args) throws ClassNotFoundException {
         DatabaseConnection DBC = new DatabaseConnection();
         DBC.openConnection();
+        
+        DatabaseScheme DBSch = new DatabaseScheme(DBC);
+        DBSch.createScheme();
+        
         DBC.closeConnection();
     }
 }
