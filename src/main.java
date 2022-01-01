a611924ab8fc0d0db4fdd7ed61990a5dec6d9080
JacekJanur <jacekjanur1@gmail.com>
2022-01-01 14:15:01 +0100

    Added basic running program logic

--- src/main.java
+++ src/main.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 import classes.DatabaseConnection;
 import classes.DatabasePrint;
 import classes.DatabasePrintPerson;
@@ -15,6 +17,8 @@ import classes.Responsibility;
 import classes.ResponsibilitySave;
 import classes.ResponsibilitySaveToDB;
 
+
+
 public class main
 {
 	public static void main(String[] args) throws ClassNotFoundException{
@@ -27,26 +31,55 @@ public class main
         PersonSave personS = new PersonSave(new Person("Jacek", "Janur", "1997-10-05", 1, 4000), new PersonSaveToDB(DBC));
         personS.Save();
         
-        PersonPrint personP = new PersonPrint(new Person("Jacek", "Janur", "1997-10-05", 1, 4000), new PersonPrintToConsole());
-        personP.print();
+//        PersonPrint personP = new PersonPrint(new Person("Jacek", "Janur", "1997-10-05", 1, 4000), new PersonPrintToConsole());
+//        personP.print();
+//        
+//        DatabasePrint DBP = new DatabasePrint(DBC, new DatabasePrintPerson());
+//        DBP.print();
         
-        DatabasePrint DBP = new DatabasePrint(DBC, new DatabasePrintPerson());
-        DBP.print();
-        
-        ResponsibilitySave resS = new ResponsibilitySave(new Responsibility("Cleaning office"), new ResponsibilitySaveToDB(DBC));
-        resS.save();
-        resS = new ResponsibilitySave(new Responsibility("Cleaning Toilets"), new ResponsibilitySaveToDB(DBC));
-        resS.save();
+        new ResponsibilitySave(new Responsibility("Cleaning office"), new ResponsibilitySaveToDB(DBC)).save();
+        new ResponsibilitySave(new Responsibility("Cleaning Toilets"), new ResponsibilitySaveToDB(DBC)).save();
         
         new PersonResponsibilitySave(1, 1).save(DBC);
         new PersonResponsibilitySave(1, 2).save(DBC);
         
-        DBP = new DatabasePrint(DBC, new DatabasePrintResponsibilities());
-        DBP.print();
+//        DBP = new DatabasePrint(DBC, new DatabasePrintResponsibilities());
+//        DBP.print();
         
-        DBP = new DatabasePrint(DBC, new DatabasePrintPersonResponsibilities());
-        DBP.print();
+//        new DatabasePrint(DBC, new DatabasePrintPersonResponsibilities()).print();
         
+        Scanner keyboard = new Scanner(System.in);
+        boolean lo = true;
+        while(lo)
+        {
+        	System.out.print("\nSelect number 1-3: \n"
+        			+ "1: Show all workers \n"
+        			+ "2: Show all responsibilities \n"
+        			+ "3: Show workers responsibilities \n"
+        			+ "4: Quit \n");
+        	System.out.print("Your number: ");
+        	int number = keyboard.nextInt();
+        	System.out.print("------ \n");
+        	switch(number)
+        	{
+	        	case 1:
+	    			new DatabasePrint(DBC, new DatabasePrintPerson()).print();
+	    			break;
+	        	case 2:
+	    			new DatabasePrint(DBC, new DatabasePrintResponsibilities()).print();
+	    			break;
+	        	case 3:
+	        		new DatabasePrint(DBC, new DatabasePrintPersonResponsibilities()).print();
+	        		break;
+        		case 4: 
+        			lo = false;
+        			break;
+        	}
+        	
+        	
+        }
+        
+        keyboard.close();
         DBC.closeConnection();
     }
 }
